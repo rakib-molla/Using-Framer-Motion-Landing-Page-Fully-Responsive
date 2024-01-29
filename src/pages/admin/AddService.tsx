a1c8b54d9d4506212b61b76812eb8dc91b4b1723
rakib-molla <rakibmail1997@gmail.com>
2024-01-29 13:18:58 +0600

    service add

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { FormEvent, useState } from "react"
 
 function AddService() {
+  const[serviceName, setServiceName] = useState('');
+  const handleSubmit = (e: FormEvent)=>{
+    e.preventDefault();
+    const serviceData ={
+      name: serviceName,
+      description: 'Description',
+      device:['mackbook', 'Apple', 'Android'],
+      price: 500.0
+    }
+    console.log(serviceData)
+  }
   return (
-    <div>AddService</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input className="border" type="text" onChange={(e)=> setServiceName(e.target.value)} />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
   )
 }
 
